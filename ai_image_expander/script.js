8afb8d5aaa94d99787138df3f23d2746768246ce
James C. Tucker <hellojamestucker@gmail.com>
2023-11-11 18:01:43 -0600

    successfully retrieve a generatively-filled image from replicate

--- ai_image_expander/script.js
+++ ai_image_expander/script.js
@@ -3,16 +3,17 @@
 document.addEventListener("DOMContentLoaded", function () {
   const fileInput = document.getElementById("fileInput");
   const maskedImage = document.getElementById("maskedImage");
+  let resultImage = document.getElementById("resultImage");
   const aspect169 = document.getElementById("aspect169");
   const aspect11 = document.getElementById("aspect11");
   const aspect23 = document.getElementById("aspect23");
   const canvas = document.getElementById("canvas");
   const ctx = canvas.getContext("2d");
   const expandImageButton = document.getElementById("expandImageButton");
-  const testButton = document.getElementById("testButton");
 
   let uploadedImage = null;
   let uploadedImageFile = null;
+  let maskedDataURL = null;
   let maskFile = null;
   let aspectRatio = 16 / 9; // Default aspect ratio
 
@@ -30,10 +31,6 @@ document.addEventListener("DOMContentLoaded", function () {
       };
       reader.readAsDataURL(file);
     }
-  });
-
-  testButton.addEventListener("click", function () {
-    testServer();
   });
 
   expandImageButton.addEventListener("click", function () {
@@ -108,16 +105,16 @@ document.addEventListener("DOMContentLoaded", function () {
           uploadedImageData.data[pixelIndex + 3] !== 0;
 
         if (isUploadedImagePixel) {
-          // Set the interior (where the image sits) to white
-          maskData.data[pixelIndex] = 255; // Red channel
-          maskData.data[pixelIndex + 1] = 255; // Green channel
-          maskData.data[pixelIndex + 2] = 255; // Blue channel
-          maskData.data[pixelIndex + 3] = 255; // Alpha channel (fully opaque)
-        } else {
-          // Set the edges to black
+          // Set the interior (where the image sits) to black
           maskData.data[pixelIndex] = 0; // Red channel
           maskData.data[pixelIndex + 1] = 0; // Green channel
           maskData.data[pixelIndex + 2] = 0; // Blue channel
+          maskData.data[pixelIndex + 3] = 255; // Alpha channel (fully opaque)
+        } else {
+          // Set the edges to white
+          maskData.data[pixelIndex] = 255; // Red channel
+          maskData.data[pixelIndex + 1] = 255; // Green channel
+          maskData.data[pixelIndex + 2] = 255; // Blue channel
           maskData.data[pixelIndex + 3] = 255; // Alpha channel (fully opaque)
         }
       }
@@ -134,7 +131,7 @@ document.addEventListener("DOMContentLoaded", function () {
     maskCtx.putImageData(maskData, 0, 0);
 
     // Convert the mask canvas to a Data URL
-    const maskedDataURL = maskCanvas.toDataURL("image/png"); // Specify the desired image format
+    maskedDataURL = maskCanvas.toDataURL("image/png"); // Specify the desired image format
 
     // add url to maskedImage element
     maskedImage.src = maskedDataURL;
@@ -169,51 +166,27 @@ document.addEventListener("DOMContentLoaded", function () {
     return new Blob([ab], { type: mimeString });
   }
 
-  //   function expandImage() {
-  //     const requestData = {
-  //       mask: "https://replicate.delivery/pbxt/HtGQBqO9MtVbPm0G0K43nsvvjBB0E0PaWOhuNRrRBBT4ttbf/mask.png",
-  //       image:
-  //         "https://replicate.delivery/pbxt/HtGQBfA5TrqFYZBf0UL18NTqHrzt8UiSIsAkUuMHtjvFDO6p/overture-creations-5sI6fQgYIuo.png",
-  //       prompt:
-  //         "Please fill in the rest of the image to naturally expand what is already present.",
-  //       num_outputs: 1,
-  //       guidance_scale: 7.5,
-  //       prompt_strength: 0.8,
-  //       num_inference_steps: 25,
-  //     };
+  function expandImage() {
+    const payload = {
+      image: uploadedImage.src,
+      mask: maskedDataURL,
+    };
 
-  //     const url = "https://api.replicate.com/v1/predictions";
+    const headers = {
+      "Content-Type": "application/json",
+    };
 
-  //     const headers = new Headers();
-  //     headers.append("Authorization", `Token ${REPLICATE_KEY}`);
-  //     headers.append("Content-Type", "application/json");
+    const options = {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(payload),
+    };
 
-  //     const requestOptions = {
-  //       method: "POST",
-  //       headers: headers,
-  //       body: JSON.stringify(requestData),
-  //     };
-
-  //     fetch(url, requestOptions)
-  //       .then((response) => {
-  //         if (!response.ok) {
-  //           throw new Error(`HTTP error! Status: ${response.status}`);
-  //         }
-  //         return response.json(); // Parse the response as JSON
-  //       })
-  //       .then((data) => {
-  //         // Handle the API response data here
-  //         console.log("API Response:", data);
-  //       })
-  //       .catch((error) => {
-  //         // Handle errors
-  //         console.error("API Error:", error);
-  //       });
-  //   }
-
-  function testServer() {
-    fetch("http://localhost:8000/predictions/")
+    fetch("http://localhost:8000/api/v1/predictions/", options)
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => {
+        console.log(data);
+        resultImage.src = data[0];
+      });
   }
 });
